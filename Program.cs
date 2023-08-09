c0de65ce545ce1237f7c453079ef3a49f27a4424
Dursun Dashdamirov <dursun.dashdemirov.std@bhos.edu.az>
2023-08-09 21:13:05 +0400

    Stack
    Quick overview of stack in c#

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+
+
+
+namespace StackExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack astack = new Stack();
+
+
+            Stack<int> bstack = new Stack<int>();
+            // Inner elements can be predefined, in this case we cannot push string or float to bstack
+
+            // Adding elements
+            astack.Push(1);
+            astack.Push(2);
+            astack.Push(3.0);
+            astack.Push("Hello");
+
+            //Droping the element on top of box
+            //astack.Pop();
+
+            foreach (var data in astack)
+            {
+                Console.WriteLine(data);
+                // Output is in sequence of Hello, 3, 2, 1 => Last in First Out
+                // Like dropping marbels in a square box
+            }
+
+            //Return the top element of stack without dropping it  
+            astack.Peek();
+
+
+
+        }
+
+    }
+
+
+
+}
