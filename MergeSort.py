d5cc8811c0dba3fa9a6fe5fd2bc050272b672538
Dursun Dashdamirov <dursun.dashdemirov.std@bhos.edu.az>
2023-08-29 13:02:54 +0400

    Merge sort in python

---
+++ MergeSort.py
@@ -0,0 +1,36 @@
+
+
+def merge_sort(arr):
+    if len(arr) > 1:
+        left_arr = arr[:len(arr)//2]
+        right_arr = arr[len(arr)//2:]
+
+        merge_sort(left_arr)
+        merge_sort(right_arr)
+
+        #merge
+        i,j = 0,0
+        k = 0
+        while i < len(left_arr) and j < len(right_arr):
+            if left_arr[i] < right_arr[j]:
+                arr[k] = left_arr[i]
+                i+=1
+                
+            else:
+                arr[k] = right_arr[j]
+                j += 1
+            k+=1
+
+        while i < len(left_arr):
+            arr[k] = left_arr[i]
+            i+=1
+            k+=1
+
+        while j <  len(right_arr):
+            arr[k] = right_arr[j]
+            j+=1
+            k+=1    
+
+arrtosort = [4,23,6,8,11,3,5,6,7,8]
+merge_sort(arrtosort)
+print(arrtosort)
